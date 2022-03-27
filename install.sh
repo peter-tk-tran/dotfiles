3fc58b098b7a2218bf9b1ccdbab47c4dbccee7d5
Peter Tran <ptt006@ucsd.edu>
2022-03-26 22:10:00 -0700

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
+setup_vim() {
+    # installing neovim nightly
+    if hash nvim 2>/dev/null; then
+        echo "nvim exists."
+    else
+        echo "Installing nvim"
+        brew install neovim
+    fi
 
-# installing neovim nightly
-if hash nvim 2>/dev/null; then
-    echo "nvim exists."
-else
-    echo "Installing nvim"
-    brew install neovim
-fi
+    # setting up packer; this manages neovim plugins
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-mv ~/.vimrc ~/.vimrc.old
-cp ./.vimrc ~/.vimrc
+    # moving init.vim
+    mv ~/.vimrc ~/.vimrc.old
+    cp ./.vimrc ~/.vimrc
 
-rm -r ~/.config/nvim
-cp -r ./nvim ~/.config/nvim
-vim +PlugInstall +qall
-nvim +PackerSync
+    mv ~/.config/nvim ~/.config/old_nvim
+    cp -r ./nvim ~/.config/nvim
 
+    # Install vim plugins
+    vim +PlugInstall +qall
+
+    # Install neovim plugins
+    nvim +PackerSync
+}
+
+setup_python_env() {
+    # Python setup
+    if hash pyright 2>/dev/null; then
+        echo "pyright exists"
+    else
+        npm i -g pyright
+    fi
+}
 
 # git configuration
 git config --global alias.c "commit"
@@ -28,13 +44,5 @@ git config --global alias.p "push"
 git config --global alias.s "status"
 # TODO add git hooks logic.
 
-
-
-# Python setup
-if hash pyright 2>/dev/null; then
-    echo "pyright exists"
-else
-    npm i -g pyright
-fi
-
-echo "Installation complete. Please restart your terminal for changes to take effect."
+setup_vim
+setup_python_env
